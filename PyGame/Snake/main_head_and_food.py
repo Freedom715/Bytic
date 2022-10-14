18d468d431d0d4568bef264e07fbff5289419b2c
Svoboda21 <zotova_2003@bk.ru>
2022-10-14 11:00:44 +0300

    Snake - Documentation

--- PyGame/Snake/main_head_and_food.py
+++ PyGame/Snake/main_head_and_food.py
@@ -6,6 +6,8 @@ SIZE = WIDTH, HEIGHT = 800, 600
 SCREEN = pygame.display.set_mode(SIZE)
 CLOCK = pygame.time.Clock()
 CELL_SIZE = 25
+SNAKE_SPEED = 10
+
 
 class SnakeBody:
     def __init__(self, x, y):
@@ -15,59 +17,55 @@ class SnakeBody:
         self.color = (0, 100, 0)
         pygame.draw.rect(SCREEN, self.color, (self.x, self.y, CELL_SIZE, CELL_SIZE))
 
-    def move(self):
+    def draw(self):
         pygame.draw.rect(SCREEN, self.color, (self.x, self.y, CELL_SIZE, CELL_SIZE))
 
+
 def game_loop():
+    x1, x1_change = WIDTH // 2 // CELL_SIZE * CELL_SIZE, 0
+    y1, y1_change = HEIGHT // 2 // CELL_SIZE * CELL_SIZE, 0
+    snake_list = []
+    snake_len = 1
+    food_x = randint(0, (WIDTH - CELL_SIZE) // CELL_SIZE) * CELL_SIZE
+    food_y = randint(0, (HEIGHT - CELL_SIZE) // CELL_SIZE) * CELL_SIZE
     game_over = False
     while not game_over:
-        x1, x1_change = WIDTH // 2 // CELL_SIZE * CELL_SIZE, 0
-        y1, y1_change = HEIGHT // 2 // CELL_SIZE * CELL_SIZE, 0
-        food_x = randint(0, (WIDTH - CELL_SIZE) // CELL_SIZE) * CELL_SIZE
-        food_y = randint(0, (HEIGHT - CELL_SIZE) // CELL_SIZE) * CELL_SIZE
-        snake_list = []
-        snake_len = 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
             if event.type == pygame.KEYDOWN and x1 % CELL_SIZE == 0:
                 if event.key == pygame.K_UP:
                     x1_change, y1_change = 0, -CELL_SIZE
-                    direction = 0
                 elif event.key == pygame.K_RIGHT:
                     x1_change, y1_change = CELL_SIZE, 0
-                    direction = 1
                 elif event.key == pygame.K_DOWN:
                     x1_change, y1_change = 0, CELL_SIZE
-                    direction = 2
                 elif event.key == pygame.K_LEFT:
                     x1_change, y1_change = -CELL_SIZE, 0
-                    direction = 3
-            x1 += x1_change
-            y1 += y1_change
-            if x1 >= WIDTH:
-                x1 = 0
-            elif x1 < 0:
-                x1 = WIDTH
-            if y1 >= HEIGHT:
-                y1 = 0
-            elif y1 < 0:
-                y1 = HEIGHT
+        x1 += x1_change
+        y1 += y1_change
+        if x1 >= WIDTH:
+            x1 = 0
+        elif x1 < 0:
+            x1 = WIDTH
+        if y1 >= HEIGHT:
+            y1 = 0
+        elif y1 < 0:
+            y1 = HEIGHT
+        SCREEN.fill((0, 150, 0))
         pygame.draw.rect(SCREEN, (190, 0, 0), [food_x, food_y, CELL_SIZE, CELL_SIZE])
         snake_list.append(SnakeBody(x1, y1))
         if len(snake_list) > snake_len:
             del snake_list[0]
         for elem in snake_list:
-            elem.move()
-            print("Y")
-        for x in snake_list[:-1]:
-            x.head = False
-            x.color = (0, 200, 0)
-            if (x.x, x.y) == (snake_list[-1].x, snake_list[-1].y):
-                game_loop()
-        SCREEN.fill((0, 150, 0))
+            elem.draw()
+        if x1 == food_x and y1 == food_y:
+            food_x = randint(0, (WIDTH - CELL_SIZE) // CELL_SIZE) * CELL_SIZE
+            food_y = randint(0, (HEIGHT - CELL_SIZE) // CELL_SIZE) * CELL_SIZE
+            snake_len += 1
         pygame.display.update()
-        CLOCK.tick(10)
+        CLOCK.tick(SNAKE_SPEED)
+
 
 pygame.init()
 game_loop()
--- PyGame/Snake/main_window.py
+++ PyGame/Snake/main_window.py
@@ -6,18 +6,13 @@ screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 CELL_SIZE = 25
 
+game_over = False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
 
-def game_loop():
-    game_over = False
-    while not game_over:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-
-        screen.fill((0, 150, 0))
-        pygame.display.update()
-        clock.tick(10)
-
-
-
-game_loop()
+    screen.fill((0, 150, 0))
+    pygame.display.update()
+    clock.tick(10)
+pygame.quit()
